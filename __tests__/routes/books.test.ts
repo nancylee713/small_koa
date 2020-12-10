7c195131ff77fd6c4faa652d90bb3ed1eaa4c645
nancylee713 <srlee713@gmail.com>
2020-12-10 14:11:33 +0900

    Mock with jest continued

--- __tests__/routes/books.test.ts
+++ __tests__/routes/books.test.ts
@@ -13,13 +13,13 @@ describe("routes/books", () => {
 
   const books = [
     "아몬드",
-    // "모모",
+    "모모",
   ]
 
   books.forEach((book: string) => {
-    it.only(`should allow adding books to the list - ${book}`, async () => {
-
-      const mockGet = jest.fn((list: string) => Promise.resolve([book]));
+    it(`should allow adding books to the list - ${book}`, async () => {
+      const myList: string[] = [];
+      const mockGet = jest.fn((list: string) => Promise.resolve(myList));
 
       storage.redisStorage = jest.fn(() => {
         return {
@@ -36,17 +36,96 @@ describe("routes/books", () => {
       expect(response.status).toEqual(201);
       expect(response.type).toEqual("application/json");
       expect(response.body).toEqual({
-        books: [
-          book,
-        ]
+        books: myList
       });
 
       expect(mockGet).toHaveBeenCalled();
     });
   });
 
+  it('should not allow adding the same book to the list', async () => {
+    const list_of_books: string[] = [];
+    const mockGet = jest.fn((list: string) => Promise.resolve(list_of_books));
+    const mockAdd = jest.fn((list: string, title: string) => {
+      list_of_books.push(title);
+      return list_of_books.length > 0;
+    });
 
-  xit('should return a validation failure if the book data is incorrect', async () => {
+    storage.redisStorage = jest.fn(() => {
+      return {
+        get: mockGet,
+        add: mockAdd,
+        remove: (list: string) => Promise.resolve(false),
+      }
+    });
+
+    const book1 = { title: "book1" };
+    const response1 = await request(server)
+      .post("/books")
+      .send(book1);
+
+    expect(response1.status).toEqual(201);
+
+    const response2 = await request(server)
+      .post("/books")
+      .send(book1);
+
+    expect(response2.status).toEqual(400);
+    expect(response2.type).toEqual("application/json");
+    expect(response2.body).toEqual({
+      "status": "error",
+      "data": "The same book is already in the list!"
+    });
+  });
+
+  it('should keep track of all books added to the list', async () => {
+
+    const list_of_books: string[] = [];
+    const mockGet = jest.fn((list: string) => Promise.resolve(list_of_books));
+    const mockAdd = jest.fn((list: string, title: string) => {
+      list_of_books.push(title);
+      return list_of_books.length > 0;
+    });
+
+
+    // Define the mock function that have exactly the same signature as found on the interface  
+    storage.redisStorage = jest.fn(() => {
+      return {
+        get: mockGet,
+        add: mockAdd,
+        remove: (list: string) => Promise.resolve(false),
+      }
+    });
+
+    const book1 = { title: "The giver" };
+    const response1 = await request(server)
+      .post("/books")
+      .send(book1);
+
+    expect(response1.status).toEqual(201);
+    expect(response1.type).toEqual("application/json");
+    expect(response1.body).toEqual({
+      books: [
+        book1.title,
+      ]
+    })
+
+    const book2 = { title: "The son" };
+    const response2 = await request(server)
+      .post("/books")
+      .send(book2);
+
+    expect(response2.status).toEqual(201);
+    expect(response2.type).toEqual("application/json");
+    expect(response2.body).toEqual({
+      books: list_of_books
+    })
+
+    expect(mockAdd).toHaveBeenCalled();
+    expect(mockGet).toHaveBeenCalled();
+  }); 
+
+  it('should return a validation failure if the book data is incorrect', async () => {
     const response = await request(server)
       .post("/books")
       .send({ title: ""});
--- src/routes/books.ts
+++ src/routes/books.ts
@@ -31,12 +31,27 @@ router.post(`/books`, async (ctx: Context) => {
       return ctx;
     }
 
-    // test mock storage
-    console.log('route storage', storage);
-    
+    // save the new book to storage
+    const store = storage.redisStorage();
+    const list_name = "my_book_list";
+
+    const my_list = await store.get(list_name)    
+        
+    if (my_list.length > 0 && my_list.includes(addBookRequest.title)) {
+      ctx.status = 400;
+      ctx.body = {
+        status: 'error',
+        data: "The same book is already in the list!"
+      }
+
+      return ctx;
+    }
+
+    await store.add(list_name, addBookRequest.title);
+
     ctx.status = 201;
     ctx.body = {
-      books: await storage.redisStorage().get('my_test_list')
+      books: await store.get(list_name)
     }
   } catch (e) {
     console.error(e);
